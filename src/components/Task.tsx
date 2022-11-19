06e8813375888393b40b0e108cf135a7929e5529
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2022-11-19 20:23:54 -0300

    Funcionamento

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,15 +2,33 @@ import { Trash } from 'phosphor-react';
 
 import styles from './Task.module.css';
 
-export function Task() {
+interface TaskProps {
+  task: {
+    id: string;
+    content: string;
+    done: boolean;
+  },
+  onDeleteTask(id: string): void;
+  onChanceTaskDoneStatus(id: string): void;
+}
+
+export function Task({task, onDeleteTask, onChanceTaskDoneStatus}: TaskProps) {
+  function handleDeleteTask() {
+    onDeleteTask(task.id);
+  }
+
+  function handleChanceTaskDoneStatus() {
+    onChanceTaskDoneStatus(task.id);
+  }
+
   return (
     <div className={styles.task}>
       <label className={styles.container}>
-        <input type="checkbox" />
+        <input type="checkbox" checked={task.done} onChange={handleChanceTaskDoneStatus} />
         <span className={styles.checkmark}></span>
       </label>
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-      <button title="Deletar tarefa">
+      <p>{task.content}</p>
+      <button title="Deletar tarefa" onClick={handleDeleteTask}>
         <Trash size={20} />
       </button>
     </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,71 @@
 import { PlusCircle, ClipboardText } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 import { Task } from './Task';
 
 import styles from './TaskList.module.css';
 
+interface Task {
+  id: string;
+  content: string;
+  done: boolean;
+}
+
 export function TaskList() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskText, setNewTaskText] = useState('');
+
+  function handleNewTaskTextChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+    
+    setTasks(tasks => [{
+      id: uuid(),
+      content: newTaskText,
+      done: false
+    }, ...tasks]);
+    setNewTaskText('');
+  }
+
+  function countDoneTasks() {
+    return tasks.reduce(function(acc, task) {
+      return task.done ? acc + 1 : acc
+    }, 0);
+  }
+
+  function handleDeleteTask(id: string) {
+    const tasksWithoutDeletedOne = tasks.filter(task => {
+      return task.id !== id;
+    });
+
+    setTasks(tasksWithoutDeletedOne);
+  }
+
+  function handleChanceTaskDoneStatus(id: string) {
+    const tasksWithChangedOne = tasks.map(task => {
+      if(task.id === id) task.done = !task.done;
+      return task;
+    });
+
+    setTasks(tasksWithChangedOne);
+  }
+
+  const isNewTaskTextEmpty = newTaskText.length === 0;
+
   return (
     <div className={styles.wrapper}>
-      <form>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button type="submit">
+      <form onSubmit={handleCreateNewTask}>
+        <input 
+          type="text" 
+          placeholder="Adicione uma nova tarefa"
+          value={newTaskText}
+          onChange={handleNewTaskTextChange}
+        />
+        <button type="submit" disabled={isNewTaskTextEmpty}>
           Criar <PlusCircle size={16} weight="bold" />
         </button>
       </form>
@@ -16,25 +73,34 @@ export function TaskList() {
       <div className={styles.info}>
         <div>
           <strong className={styles.created}>Tarefas criadas</strong>
-          <span>0</span>
+          <span>{tasks.length}</span>
         </div>
         <div>
           <strong className={styles.done}>Concluídas</strong>
-          <span>0</span>
+          <span>{tasks.length <= 0 ? 0 : `${countDoneTasks()} de ${tasks.length}`}</span>
         </div>
       </div>
 
-      <div className={styles.emptyList}>
-        <ClipboardText size={56} weight="thin" />
-        <div>
-          <strong>Você ainda não tem tarefas cadastradas</strong>
-          <p>Crie tarefas e organize seus itens a fazer</p>
+      { tasks.length <= 0 ? (
+        <div className={styles.emptyList}>
+          <ClipboardText size={56} weight="thin" />
+          <div>
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
         </div>
-      </div>
+      ) : (
+        tasks.map(task => { return (
+          <Task 
+            key={task.id} 
+            task={task} 
+            onDeleteTask={handleDeleteTask} 
+            onChanceTaskDoneStatus={handleChanceTaskDoneStatus}
+          />
+        ) })
+      ) }
 
-      {/* <Task />
-      <Task />
-      <Task /> */}
+
 
     </div>
   )
